06c497e6715c1b83f45949f82ae890c5e6b77c0d
Rails Abam <railsdevabam@gmail.com>
2024-02-19 08:01:10 +0100

    new changes

--- .vscode/Class Project/NoteBook/Class/NoteBook/Class/Student.java
+++ .vscode/Class Project/NoteBook/Class/NoteBook/Class/Student.java
@@ -1,10 +1,8 @@
 
-package NoteBook.Class Project;
-
 import java.util.*;
 public class Student {
     private String name;
-    private final int id;
+    private int id;
     private int age;
     private char grade;
 
@@ -73,63 +71,5 @@ public static void viewStudentDetails( final int id){
 
 
 
-public class StudentmanagementRecords{
-    public static void main(final String[] args, int id){
-        final Scanner scanner = new Scanner(System.in);
-           
-        while (true){//check 
-            System.out.println("Student management records system");
-            System.out.println("1. Please add Student");
-            System.out.println("2. Please update Student Information");
-            System.out.println("3. Please View Student details");
-            System.out.println("1. Please Exit");
-            System.out.println("1. Please Enter your Option");
 
-
-            final int Option = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (Option) {
-            case 1:
-                System.out.println("Please enter your Student Name: ");
-                final String name = scanner.nextLine();
-                System.out.println("Please enter student age: ");
-                final int age = scanner.nextInt();
-                System.out.println("Please enter Student grade: "); 
-                final char grade = scanner.next().charAt(0);
-                StudentManagement.addStudent(name,id, age, grade);
-                    break;
-            
-            case 2:
-                 System.out.println("Please enter Student ID you want to update: ");
-                 final int idToUpdate = scanner.nextInt();
-                 scanner.nextLine();
-                 System.out.println("Please enter your Student Name: ");
-                 final String updatedName = scanner.nextLine();
-                 System.out.println("Please enter your Student age: ");
-                 final int updatedAge = scanner.nextInt();
-                 System.out.println("Please enter your Student Grade: ");
-                 final char updatedGrade = scanner.next().charAt(0);
-                 StudentManagement.updatedStudentRecord(updatedName, idToUpdate, updatedAge, updatedGrade);
-                 break;
-
-            case 3:
-                 System.out.println("Please enter Student ID you want to view: ");
-                 final int idToView = scanner.nextInt();
-                 StudentManagement.viewStudentDetails(idToView);
-                 break;
-            case 4:
-                 System.out.println("You are about exiting the Program, Goodbye!!!");
-                 System.exit(0);
-            default:
-            System.out.println("Please Ypur Options is not valid!!");
-
-
-            }
-        }
-    }
-
-}
-
-
-
+   
